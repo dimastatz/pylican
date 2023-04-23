46da5fada8459e307a267b2a34cbbb8df8c6dfad
dimastatz <dima.statz@gmail.com>
2023-04-23 21:55:01 +0300

    change source code

---
+++ ludus/learn_flow.py
@@ -0,0 +1,6 @@
+"""func dummy returns num"""
+
+
+def dummy(x: int) -> int:
+    """inc number"""
+    return x + 1
---
+++ tests/test_flow.py
@@ -0,0 +1,5 @@
+from ludus.learn_flow import dummy
+
+
+def test_flow():
+    assert dummy() == 1
